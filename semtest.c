2ae11116056a999969cbdc319226c4463fe68f61
Moises Higuerey <mhiguerey@outlook.com>
2022-05-11 15:50:08 -0600

    Segmentation and pagination funcs done + comments

--- semtest.c
+++ semtest.c
@@ -9,107 +9,221 @@
 #include <sys/wait.h>
 #include <pthread.h>
 
-// SOURCES:
-// https://www.geeksforgeeks.org/use-posix-semaphores-c/
-// https://stackoverflow.com/questions/40181096/c-linux-pthreads-sending-data-from-one-thread-to-another-using-shared-memory-gi
-
-#define SIZE 5
 key_t key = 12345678;
 sem_t mutex;
 
-int counterGlobal = 0;
+int counterGlobal = 0; // Counter for PID of processes, would be managed by process structure
+int segSize;           // This would be generated randomly by the init function
+int SIZE;              // Size of shared memory, given by user input in init function
 
-void Process(void *arg)
+void segmentProcess(void *arg)
 {
-    int counterLocal = counterGlobal;
-    counterGlobal++;
-    int shmid;
-    int full = 0; // Flag to find space
 
-    sem_wait(&mutex);
-    shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666);
-    int *array = (int *)shmat(shmid, 0, 0);
-    printf("Entro a arreglo %d\n", counterLocal);
-    for (int pos = 0; pos < SIZE; pos++)
+    int counterLocal = counterGlobal; // Save current PID counter locally
+    counterGlobal++;                  // Increase global PID counter
+    int shmid;
+    int full = 0;     // Flag for memory availability ; 0 = space was not found, 1 = space was found
+    int segCount = 0; // Counter to check if there is enough continuous space
+
+    /*
+     * Here is where the sync algorithm would be implemented
+     * Before waiting at the semaphore, each process would check if it's their turn
+     */
+
+    sem_wait(&mutex);                                          // Send wait signal to semaphore when ready
+    printf("-------Busca espacio %d-------\n", counterLocal);  // PRINT FOR TESTING PURPOSES
+    shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666); // Get shared memory
+    int *array = (int *)shmat(shmid, 0, 0);                    // Map memory to array
+    // Loop to check for available memory
+    for (int pos = 0; pos < SIZE; pos++) // Here, SIZE would be replaced by the amount of segments
     {
-        if (array[pos] == -1)
+        if (array[pos] == -1) // If the memory space is available
         {
-            array[pos] = counterLocal;
-            full = 1;
-            break;
+            segCount++;              // Increase segment count
+            if (segCount == segSize) // If the segment count is equal to the required space
+            {
+                while (segCount != 0) // Fill the available space
+                {
+                    segCount--;
+                    array[pos - segCount] = counterLocal;
+                }
+                full = 1; // Memory wasn't full
+                break;
+            }
+        }
+        else // If memory space isn't available at current index
+        {
+            segCount = 0; // Reset segment count
         }
     }
-    printf("Arreglo actual %d\n", counterLocal);
-    for (int pos = 0; pos < SIZE; pos++)
+    printf("Arreglo actual %d\n", counterLocal); // PRINT FOR TESTING PURPOSES
+    for (int pos = 0; pos < SIZE; pos++)         // Print memory space status, FOR TESTING PURPOSES
     {
         printf("%d ", array[pos]);
     }
-    printf("\n");
+    printf("\n"); // PRINT FOR TESTING PURPOSES
 
-    if (full)
+    if (full) // If there was memory available
     {
-        printf("Encontro espacio %d\n", counterLocal); // Se escribe en bitacora
+        // NOT IMPLEMENTED
+        // Write to log relevant information
+        printf("------Encontro espacio %d-----\n", counterLocal); // PRINT FOR TESTING PURPOSES
     }
-    else
+    else // If there was no memory available
     {
-        printf("No encontro espacio %d\n", counterLocal); // Se escribe en bitacora
-        shmdt((void *)array);
-        sem_post(&mutex);
-        return;
+        // NOT IMPLEMENTED
+        // Write to log relevant information
+        printf("----No encontro espacio %d----\n", counterLocal); // PRINT FOR TESTING PURPOSES
+        shmdt((void *)array);                                     // Detach memory segment
+        sem_post(&mutex);                                         // Set semaphore to ready state
+        return;                                                   // Process dies
     }
-    shmdt((void *)array);
-    sem_post(&mutex);
+    shmdt((void *)array); // Detach memory segment
+    sem_post(&mutex);     // Set semaphore to ready state
 
-    sleep(6);
+    sleep(11); // Sleep amount would be defined by a random from the process creator
 
-    sem_wait(&mutex);
-    shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666);
-    array = (int *)shmat(shmid, 0, 0);
-    printf("Liberando espacio %d\n", counterLocal);
-    for (int pos = 0; pos < SIZE; pos++)
+    sem_wait(&mutex);                                          // Wait for ready signal
+    shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666); // Get id of memory space
+    array = (int *)shmat(shmid, 0, 0);                         // Map shared memory to an array
+    printf("-----Liberando espacio %d-----\n", counterLocal);  // PRINT FOR TESTING PURPOSES
+    for (int pos = 0; pos < SIZE; pos++)                       // Set memory spaces filled by process to -1
     {
         if (array[pos] == counterLocal)
         {
             array[pos] = -1;
-            full = 1;
+        }
+    }
+    printf("Arreglo actual %d\n", counterLocal); // PRINT FOR TESTING PURPOSES
+    for (int pos = 0; pos < SIZE; pos++)         // PRINT FOR TESTING PURPOSES
+    {
+        printf("%d ", array[pos]);
+    }
+    printf("\n"); // PRINT FOR TESTING PURPOSES
+
+    // NOT IMPLEMENTED YET
+    // Write to log relevant information
+    printf("-------Libero espacio %d------\n", counterLocal); // PRINT FOR TESTING PURPOSES
+    shmdt((void *)array);                                     // Detach memory segment
+    sem_post(&mutex);                                         // Set semaphore to ready state
+}
+
+void pageProcess(void *arg)
+{
+    int counterLocal = counterGlobal; // Save current PID counter locally
+    counterGlobal++;                  // Increase global PID counter
+    int shmid;
+    int full = 0; // Flag for memory availability ; 0 = space was not found, 1 = space was found
+
+    /*
+     * Here is where the sync algorithm would be implemented
+     * Before waiting at the semaphore, each process would check if it's their turn
+     */
+
+    sem_wait(&mutex);                                          // Send wait signal to semaphore when ready
+    printf("-------Busca espacio %d-------\n", counterLocal);  // PRINT FOR TESTING PURPOSES
+    shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666); // Get memory space
+    int *array = (int *)shmat(shmid, 0, 0);                    // Map shared memory space to array
+    // Loop to check for available memory
+    for (int pos = 0; pos < SIZE; pos++) // Here, SIZE would be replaced by the amount of pages
+    {
+        if (array[pos] == -1) // If memory is available
+        {
+            array[pos] = counterLocal; // Fill memory
+            full = 1;                  // Flip flag
             break;
         }
     }
-    printf("Arreglo actual %d\n", counterLocal);
+    printf("Arreglo actual %d\n", counterLocal); // PRINT FOR TESTING PURPOSES
+    for (int pos = 0; pos < SIZE; pos++)         // PRINT FOR TESTING PURPOSES
+    {
+        printf("%d ", array[pos]);
+    }
+    printf("\n"); // PRINT FOR TESTING PURPOSES
+
+    if (full) // If space was found in memory
+    {
+        // NOT IMPLEMENTED YET
+        // Write to log relevant information
+        printf("------Encontro espacio %d-----\n", counterLocal); // PRINT FOR TESTING PURPOSES
+    }
+    else // If space was not found in memory
+    {
+        // NOT IMPLEMENTED YET
+        // Write to log relevant information
+        printf("----No encontro espacio %d----\n", counterLocal); // PRINT FOR TESTING PURPOSES
+        shmdt((void *)array);                                     // Detach memory space
+        sem_post(&mutex);                                         // Set semaphore to ready state
+        return;                                                   // Process dies
+    }
+
+    shmdt((void *)array); // Detach memory space
+    sem_post(&mutex);     // Set semaphore to ready state
+
+    sleep(11); // Sleep amount would be defined by a random from the process creator
+
+    sem_wait(&mutex);                                          // Wait for ready signal
+    shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666); // Get memory space
+    array = (int *)shmat(shmid, 0, 0);                         // Map shared memory space to array
+    printf("-----Liberando espacio %d-----\n", counterLocal);  // PRINT FOR TESTING PURPOSES
+    for (int pos = 0; pos < SIZE; pos++)                       // Set memory occupied by process back to -1
+    {
+        if (array[pos] == counterLocal)
+        {
+            array[pos] = -1;
+            break;
+        }
+    }
+    printf("Arreglo actual %d\n", counterLocal); // PRINT FOR TESTING PURPOSES
     for (int pos = 0; pos < SIZE; pos++)
     {
         printf("%d ", array[pos]);
     }
-    printf("\n");
-    printf("Libero espacio %d\n", counterLocal); // En bitacora
-    shmdt((void *)array);
-    sem_post(&mutex);
+    printf("\n"); // PRINT FOR TESTING PURPOSES
+
+    // NOT IMPLEMENTED YET
+    // Write relevant information to log
+    printf("-------Libero espacio %d------\n", counterLocal); // PRINT FOR TESTING PURPOSES
+
+    shmdt((void *)array); // Detach memory space
+    sem_post(&mutex);     // Set semaphore to ready state
 }
 
 int main()
 {
-
+    // INIT FUNCTION
     int shmid;
-
-    sem_init(&mutex, 0, 1);
-    shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666);
-    int *array = (int *)shmat(shmid, 0, 0);
-    for (int pos = 0; pos < SIZE; pos++)
+    printf("Enter shared memory size: ");
+    scanf("%d", &SIZE); // Ask for shared memory size from user
+    printf("\n\n");
+    sem_init(&mutex, 0, 1);                                    // initilalize semaphore
+    shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666); // Create shared memory space
+    int *array = (int *)shmat(shmid, 0, 0);                    // Map memory space to array
+    for (int pos = 0; pos < SIZE; pos++)                       // Initialize empty memory space with -1
     {
         array[pos] = -1;
     }
+    // END OF INIT FUNCTION
+
+    // TESTING AREA
+    // This part would be done by the process creator
+    // segmentProcess or pageProcess would depend on user input
+    segSize = 2; // Defined by random in process creator
     pthread_t t1;
-    while (counterGlobal != 20)
+    while (counterGlobal != 10)
     {
         pthread_t t2;
-        pthread_create(&t2, NULL, Process, NULL);
-        sleep(1);
+        pthread_create(&t2, NULL, segmentProcess, NULL);
+        sleep(2); // Fixed sleep amount, defined by random in process creator
     }
 
-    pthread_create(&t1, NULL, Process, NULL);
+    pthread_create(&t1, NULL, segmentProcess, NULL);
     pthread_join(t1, NULL);
-    sem_destroy(&mutex);
+    // END OF TESTING AREA
 
+    // Liberate semaphore memory, this would be done by the process finalizer
+    sem_destroy(&mutex);
+    // Liberate shared memory space, this would be done by the process finalizer
     shmctl(shmid, IPC_RMID, NULL);
 
     return 0;
