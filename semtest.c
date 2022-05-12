45961db2c7febe6e9738e4a1dc24bdc2788b508a
JAMares <josuegerardojgam885@gmail.com>
2022-05-12 09:30:30 -0600

    struct

--- semtest.c
+++ semtest.c
@@ -16,6 +16,71 @@ int counterGlobal = 0; // Counter for PID of processes, would be managed by proc
 int segSize;           // This would be generated randomly by the init function
 int SIZE;              // Size of shared memory, given by user input in init function
 
+
+// Process PCN
+struct PCB
+{
+	int pId;
+    int state;
+    /*
+    Defintion of the state
+    =>0-Block process
+    =>1-Search memory
+    =>2-Process in memory
+    =>3-Death process
+    =>4-Finish process
+    */
+};
+
+// List nodes
+struct Node
+{
+	struct Node *next;
+	struct PCB process;
+};
+
+// Queue structure
+struct Queue
+{
+	struct Node *first;
+	struct Node *last;
+};
+
+// Inserts process into the queue
+int insertProcess(struct Queue *q, struct PCB pcb)
+{
+	if (q->first == NULL)
+	{
+		q->first = (struct Node *)malloc(sizeof(struct Node));
+		q->first->next = NULL;
+		q->first->process = pcb;
+		q->last = q->first;
+	}
+	else
+	{
+		struct Node *tmp = (struct Node *)malloc(sizeof(struct Node));
+		tmp->next = NULL;
+		tmp->process = pcb;
+		q->last->next = tmp;
+		q->last = tmp;
+	}
+	return 0;
+}
+
+struct Node *searchProcessById(struct Queue *q, int pId)
+{
+	struct Node *node = q->first;
+	while (node != NULL)
+	{
+		if (node->process.pId == pId)
+		{
+			return node;
+		}
+		node = node->next;
+	}
+	return NULL;
+}
+
 void segmentProcess(void *arg)
 {
 
