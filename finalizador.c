16813c079923893bd79548ff7aebbae0498f0b33
valevqb03 <44345368+valevqb@users.noreply.github.com>
2022-05-17 13:07:31 -0400

    Update finalizador.c
    Permite borrar struct

--- finalizador.c
+++ finalizador.c
@@ -10,15 +10,90 @@
 #include <pthread.h>
 
 key_t key = 12345678;
+key_t structKey = 11223344;
 sem_t mutex;
 
 int SIZE = 10;              // Size of shared memory, given by user input in init function
+// Process PCN
+struct PCB
+{
+    int pId;
+    int state;
+    /*
+    Defintion of the state
+    =>0-Block process
+    =>1-Search memory
+    =>2-Process in memory
+    =>3-Death process
+    =>4-Finish process
+    */
+};
+
+// List nodes
+struct Node
+{
+    struct Node *next;
+    struct PCB process;
+};
+
+// Queue structure
+struct Queue
+{
+    struct Node *first;
+    struct Node *last;
+};
+
+// Inserts process into the queue
+int insertProcess(struct Queue *q, struct PCB pcb)
+{
+    if (q->first == NULL)
+    {
+        q->first = (struct Node *)malloc(sizeof(struct Node));
+        q->first->next = NULL;
+        q->first->process = pcb;
+        q->last = q->first;
+    }
+    else
+    {
+        struct Node *tmp = (struct Node *)malloc(sizeof(struct Node));
+        tmp->next = NULL;
+        tmp->process = pcb;
+        q->last->next = tmp;
+        q->last = tmp;
+    }
+    return 0;
+}
+
+struct Node *searchProcessById(struct Queue *q, int pId)
+{
+    struct Node *node = q->first;
+    while (node != NULL)
+    {
+        if (node->process.pId == pId)
+        {
+            return node;
+        }
+        node = node->next;
+    }
+    return NULL;
+}
+
 
 int main()
 {
-    // INIT FUNCTION
+    //For memory
     int shmid;
     shmid = shmget(key, SIZE * sizeof(int), IPC_CREAT | 0666); // Create shared memory space
+    // Liberate shared memory space, this would be done by the process finalizer
+    shmctl(shmid, IPC_RMID, NULL);
+	
+	//For struct
+    struct Node *tmp = (struct Node *)malloc(sizeof(struct Node));
+    // struct PCB *process = malloc(sizeof(struct PCB));
+    shmid = shmget(structKey, sizeof(tmp), IPC_CREAT | 0666); // Create shared memory space
+
+    tmp = (struct Node *)shmat(shmid, 0, 0); // Map shared memory space to array
+    shmdt((void *)tmp); // Detach memory space
     // Liberate shared memory space, this would be done by the process finalizer
     shmctl(shmid, IPC_RMID, NULL);
 
